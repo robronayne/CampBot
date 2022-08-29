e99d5f8c284398c1b9e15501375a17dd19cae2dd
Rob Ronayne <robmronayne@gmail.com>
2022-08-28 17:29:58 -0700

    Added login forms - Added signup and signin pages - Created password retrieval page - Check inputs for valid email

--- camp-site/src/App.js
+++ camp-site/src/App.js
@@ -1,12 +1,24 @@
-//import logo from './assets/logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home'
+import SignIn from './pages/SignIn/SignIn'
+import SignUp from './pages/SignUp/SignUp'
+import ForgotPassword from './pages/ForgotPassword/ForgotPassword'
+
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home/>}/>
+          <Route path="/sign-in" exact element={<SignIn/>}/>
+          <Route path="/sign-up" exact element={<SignUp/>}/>
+          <Route path="/forgot-password" exact element={<ForgotPassword/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
